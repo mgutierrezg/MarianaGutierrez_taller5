2d388ebd793e8c07c52abe99508ae0e61f0a940f
mgutierrezg <37335456+mgutierrezg@users.noreply.github.com>
2018-05-13 14:51:56 -0700

    hace la grafica

--- punto_3/caos.py
+++ punto_3/caos.py
@@ -5,7 +5,7 @@ datos = np.loadtxt("caos.txt")
 q2 = datos[:,0]
 p2 = datos[:,1]
 
-plt.plot(q2,p2, c= "m")
+plt.scatter(q2,p2, c= "m")
 plt.xlabel("q2")
 plt.ylabel("p2")
 plt.title("q2 vs p2, caos")
