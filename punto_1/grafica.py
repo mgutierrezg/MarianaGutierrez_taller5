d7d5b4073dba08e9eb0d5ba60f91516831e002bf
mgutierrezg <37335456+mgutierrezg@users.noreply.github.com>
2018-05-14 14:50:45 -0700

    Add files via upload

--- punto_1/grafica.py
+++ punto_1/grafica.py
@@ -8,8 +8,9 @@ datos = np.loadtxt("datos.txt").reshape(510,510,3)
 potencial = datos[:,:,0]
 Ex = datos[:,:,1]
 Ey = datos[:,:,2]
-x = np.linspace(-3,3,510)
-plt.imshow(potencial, extent = [-3,3,-3,3] ,cmap = "plasma")
+x = np.linspace(0,500,510)
+plt.imshow(potencial,cmap = "plasma")
 plt.colorbar()
 plt.streamplot(x,x,Ex,Ey)
 plt.savefig("placas.pdf")
+#extent = [-3,3,-3,3] 
